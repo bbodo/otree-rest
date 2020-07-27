bc58252f55c0e3c1d8a2d5db397ecae0c0a804c7
bodo <braegger.bodo@gmail.com>
2020-07-27 11:44:41 +0200

    participant and session viewset

--- __CUSTOM/views.py
+++ __CUSTOM/views.py
@@ -4,7 +4,8 @@ from rest_framework import viewsets, permissions
 
 from .serializers import *
 from mturk_exit_codes import models as mturk_exit_codes_models
-from otree import models as otree_models
+import otree
+#from otree import models as otree_models
 
 from pprint import pprint
 
@@ -17,19 +18,22 @@ def custom_view(request, *args, **kwargs):
     return HttpResponse('hi bro')
 
 # THE DECORATORS ARE TO USE OTREES BUILTIN LOGIN SCREEN
-@method_decorator(login_required, name='dispatch')
+# @method_decorator(login_required, name='dispatch')
 class ParticipantViewSet(viewsets.ModelViewSet):
     # THIS WOULD BE A BASIC HTML LOGIN REQUEST, not sure how to get this to work
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = otree_models.Participant.objects.all().order_by('id_in_session')
+    queryset = otree.models.Participant.objects.all().order_by('id_in_session')
     serializer_class = ParticipantSerializer
+    
 
-@method_decorator(login_required, name='dispatch')
+# @method_decorator(login_required, name='dispatch')
 class SessionViewSet(viewsets.ModelViewSet):
-    queryset = otree_models.Session.objects.all().order_by('code')
+    queryset = otree.models.Session.objects.all()
     serializer_class = SessionSerializer
 
-@method_decorator(login_required, name='dispatch')
-class  MTurkExitCodesViewSet(viewsets.ModelViewSet):
-    queryset = mturk_exit_codes_models.Subsession.objects.all().order_by('session')
-    serializer_class = MTurkExitCodesSubsessionSerializer
+# class PlayerViewSet(viewsets.ModelViewSet):
+#     queryset = otree.models.BasePlayer.objects.all()
+    # serializer_class = SessionSerializer
+
+# class GroupViewSet(viewsets.ModelViewSet):
+#     queryset = otree.models.BaseGroup.objects.all()
