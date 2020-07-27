43bade8b691028f3d0f87a38941c595855388ba1
bodo <braegger.bodo@gmail.com>
2020-07-27 11:45:25 +0200

    add delete request stub

--- rest_client.py
+++ rest_client.py
@@ -37,12 +37,12 @@ otree-rest-key: testkey
     "num_participants": 4
 }
 
-http://localhost:8000/SessionMonitor/abuu1dm4/
+http://localhost:8001/SessionMonitor/l5ws5ht5/
 
-GET http://localhost:8001/SessionMonitor/abuu1dm4/ HTTP/1.1 
+GET http://localhost:8001/SessionMonitor/l5ws5ht5/ HTTP/1.1 
 content-type: application/json
 
-GET http://localhost:8001/SessionData/abuu1dm4/ HTTP/1.1 
+GET http://localhost:8001/SessionData/l5ws5ht5/ HTTP/1.1 
 content-type: application/json
 
 POST http://localhost:8001/api/v1/participant_vars/ HTTP/1.1
@@ -60,4 +60,9 @@ otree-rest-key: testkey
 }
 
 
+DELETE http://localhost:8001/API/participants/12/?format=json HTTP/1.1
+content-type: application/json
+otree-rest-key: testkey
+
+{"url":"http://localhost:8001/API/participants/12/?format=json","vars":{},"label":null,"id_in_session":1,"payoff":"0","time_started":"2020-07-09T15:05:13.932638+02:00","mturk_assignment_id":null,"mturk_worker_id":null,"_index_in_subsessions":0,"_index_in_pages":2,"_waiting_for_ids":null,"code":"ymaz61a2","visited":true,"_last_page_timestamp":1594299919,"_last_request_timestamp":1594299919,"is_on_wait_page":false,"_current_page_name":"Guess","_current_app_name":"guess_two_thirds","_round_number":1,"_current_form_page_url":"http://localhost:8000/p/ymaz61a2/guess_two_thirds/Guess/2/","_max_page_index":13,"_browser_bot_finished":false,"_is_bot":false,"is_browser_bot":false,"session":"http://localhost:8000/API/sessions/5/?format=json"}
 """
